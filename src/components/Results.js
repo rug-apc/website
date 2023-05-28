7133af84cca174da21006e92cd7bad923be72ca6
EugenTK10 <72226901+EugenTK10@users.noreply.github.com>
2023-05-29 01:40:51 +0200

    hope it's good

--- src/components/Results.js
+++ src/components/Results.js
@@ -10,23 +10,23 @@ export default function Results() {
         <div className="table">
           <p className="text-block1 bold">BAPC 2021</p>
           <ul className="size24 ">
-            <li className="list">#43. RUG5</li>
-            <li className="list">#47. RUG1</li>
-            <li className="list">#56. RUG3</li>
-            <li className="list">#59. RUG4</li>
-            <li className="list">#60. RUG2</li>
+            <li className="list1">#43. RUG5</li>
+            <li className="list1">#47. RUG1</li>
+            <li className="list1">#56. RUG3</li>
+            <li className="list1">#59. RUG4</li>
+            <li className="list1">#60. RUG2</li>
           </ul>
         </div>
         <div className="table">
           <p className="text-block1 bold">BAPC 2022</p>
           <ul className="size24 ">
-            <li className="list">
+            <li className="list1">
               #12. Balloon Addicts, Passionate Coders
             </li>
-            <li className="list">#22. Rodina</li>
-            <li className="list">#27. The Pool Party</li>
-            <li className="list">#39. 3Guys1Segfault</li>
-            <li className="list">#40. while (true) height--;</li>
+            <li className="list1">#22. Rodina</li>
+            <li className="list1">#27. The Pool Party</li>
+            <li className="list1">#39. 3Guys1Segfault</li>
+            <li className="list1">#40. while (true) height--;</li>
           </ul>
         </div>
         </div>
@@ -34,18 +34,18 @@ export default function Results() {
         <div className="table">
           <p className="text-block1 bold">NWERC 2021</p>
           <ul className="size24 ">
-            <li className="list">#82. N-RUG2</li>
-            <li className="list">#125. N-RUG1</li>
+            <li className="list1">#82. N-RUG2</li>
+            <li className="list1">#125. N-RUG1</li>
           </ul>
         </div>
 
         <div className="table">
           <p className="text-block1 bold">NWERC 2022</p>
           <ul className="size24 ">
-            <li className="list">#86. The Pool Party</li>
-            <li className="list">#88. Balloon Addicts, Passionate Coders</li>
-            <li className="list">#99. Rodina</li>
-            <li className="list">#106. 3Guys1Segfault</li>
+            <li className="list1">#86. The Pool Party</li>
+            <li className="list1">#88. Balloon Addicts, Passionate Coders</li>
+            <li className="list1">#99. Rodina</li>
+            <li className="list1">#106. 3Guys1Segfault</li>
           </ul>
         </div>
         </div>
@@ -53,12 +53,12 @@ export default function Results() {
         <div className="table">
           <p className="text-block1 bold gold-text">NKP/BAPC winners</p>
           <ul className="size24">
-            <li className="list">1992. TBD (+UVA)</li>
-            <li className="list">1993. TBD (+UVA)</li>
-            <li className="list">1995. m38c</li>
-            <li className="list">1997. *scratch*</li>
-            <li className="list">2000. Bug Fiction</li>
-            <li className="list">2002. Bug Fiction</li>
+            <li className="list1">1992. TBD (+UVA)</li>
+            <li className="list1">1993. TBD (+UVA)</li>
+            <li className="list1">1995. m38c</li>
+            <li className="list1">1997. *scratch*</li>
+            <li className="list1">2000. Bug Fiction</li>
+            <li className="list1">2002. Bug Fiction</li>
           </ul>
         </div>
       </div>
