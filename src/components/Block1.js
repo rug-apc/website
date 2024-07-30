5a7e18fb968b77152ee56cc26cb84670c3f22398
EugenTK10 <72226901+EugenTK10@users.noreply.github.com>
2024-07-31 01:53:37 +0200

    updated regisration deadline

--- src/components/Block1.js
+++ src/components/Block1.js
@@ -8,7 +8,7 @@ const Block1 = () => {
             <div className='center flex'>
                 <p className='title-block1 b1'>Course description 2024-2025</p>
                 <p className='warning'>
-                    Registrations received by 21 July 2024 will be given full consideration although registration may
+                    Registrations received by 6 September 2024 will be given full consideration although registration may
                     be kept open beyond this date.
                 </p>
             </div>
