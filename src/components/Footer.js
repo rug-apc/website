b3b74c2ace168194340a9232559ada5ff3db0358
Arend <arendspijker@gmail.com>
2026-06-30 14:39:58 +0200

    added a last updated to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 const Footer = () => {
+  const buildTime = process.env.REACT_APP_BUILD_TIME;
+
   return (
-    <div className="footer center">
+    <div className="footer center flex">
       <p className="text-footer">
         Copyright © Algorithmic Programming Contests 2022-2026
+      </p>
+
+      <p className="text-footer">
+        Last updated:{" "}
+        {buildTime ? new Date(buildTime).toLocaleString() : "unknown"}
       </p>
     </div>
   );
