a5ca3e9d4e2fa587a06d36c0a399f59ba33a48c3
Arend <arendspijker@gmail.com>
2026-06-30 13:09:28 +0200

    changed dates and updated ocasys link + form

--- src/components/Block1.js
+++ src/components/Block1.js
@@ -1,39 +1,50 @@
-import logo from '../photo/competition.png';
+import logo from "../photo/competition.png";
 
 const Block1 = () => {
-    return (
-        <div className="max-width">
-            <div className='center flex'>
-                <p className='title-block1 b1'>Course description</p>
-                <p className='warning'>
-                    Please register as soon as possible; enrolments submitted by 22 July 2025 will receive full consideration although it may be possible to register up to the end of the first week of block 1a.
-                </p>
-            </div>
-            <div className='flex'>
-                <div className='sixty'>
-                    <p className='text-block1 small-margin'>
-                        The Algorithmic Programming Contests course is a 5 ECTS extracurricular course
-                        providing students from the University of Groningen with an introduction to the
-                        world of programming contests through a mix of (guest) lectures, labs, and participation
-                        in individual and team contests. This course was introduced in 2022-2023.
-                        <br></br>
-                        <br></br>
-                        The lectures will cover theory, algorithms, and various aspects of the contests. Some of
-                        the topics addressed in this course are: effective programming, matching problem classes
-                        with suitable solution strategies, dynamic programming, and the application of graph algorithms.
-                        <br></br>
-                        <br></br>
-                        The course is spread over three blocks, allowing the content of the lectures to be influenced
-                        by the needs of the students (e.g., in the case where a common content deficiency is identified)
-                        and to accommodate talks from renowned guest lecturers.
-                    </p>
-                </div>
-                <div className='forty'>
-                    <img className='photo-B1' src={logo} alt='Art by Ilusiku Studio'></img>
-                </div>
-            </div>
+  return (
+    <div className="max-width">
+      <div className="center flex">
+        <p className="title-block1 b1">Course description</p>
+        <p className="warning">
+          Please register as soon as possible; enrolments submitted by 12 July
+          2026 will receive full consideration although it may be possible to
+          register up to the end of the first week of block 1a.
+        </p>
+      </div>
+      <div className="flex">
+        <div className="sixty">
+          <p className="text-block1 small-margin">
+            The Algorithmic Programming Contests course is a 5 ECTS
+            extracurricular course providing students from the University of
+            Groningen with an introduction to the world of programming contests
+            through a mix of (guest) lectures, labs, and participation in
+            individual and team contests. This course was introduced in
+            2022-2023.
+            <br></br>
+            <br></br>
+            The lectures will cover theory, algorithms, and various aspects of
+            the contests. Some of the topics addressed in this course are:
+            effective programming, matching problem classes with suitable
+            solution strategies, dynamic programming, and the application of
+            graph algorithms.
+            <br></br>
+            <br></br>
+            The course is spread over three blocks, allowing the content of the
+            lectures to be influenced by the needs of the students (e.g., in the
+            case where a common content deficiency is identified) and to
+            accommodate talks from renowned guest lecturers.
+          </p>
         </div>
-    );
+        <div className="forty">
+          <img
+            className="photo-B1"
+            src={logo}
+            alt="Art by Ilusiku Studio"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Block1;
--- src/components/Block3.js
+++ src/components/Block3.js
@@ -1,74 +1,96 @@
-import React from 'react';
+import React from "react";
 
 const Block3 = () => {
+  return (
+    <div className="center max auto max-width">
+      <div>
+        <p className="title-block1" id="contact">
+          Registration
+        </p>
+      </div>
 
-    return (
-        <div className='center max auto max-width' >
-
+      <div>
+        <div className="max flex">
+          <div className="box center">
             <div>
-                <p className='title-block1' id="contact">Registration</p>
+              <p className="title-info red">Contact</p>
             </div>
-
+            <div className="block">
+              <p className="description-info">
+                <ul className="list">
+                  <li>Email address of the course: apc@rug.nl</li>
+                  <li>
+                    {" "}
+                    Course co-ordinator: Dr Revantha Ramanayake
+                    (d.r.s.ramanayake@rug.nl)
+                  </li>
+                  <li> Lecturer: Dr Ivan Bliznets (i.bliznets@rug.nl) </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div className="box center">
             <div>
-                <div className='max flex'>
-                    <div className='box center'>
-                        <div>
-                            <p className='title-info red'>Contact</p>
-                        </div>
-                        <div className='block'>
-                            <p className='description-info'>
-                                <ul className='list'>
-                                    <li>Email address of the course: apc@rug.nl</li>
-                                    <li> Course co-ordinator: Dr Revantha Ramanayake (d.r.s.ramanayake@rug.nl)</li>
-                                    <li > Lecturer: Dr Ivan Bliznets (i.bliznets@rug.nl) </li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
-                    <div className='box center'>
-                        <div>
-                            <p className='title-info red'>Registration</p>
-                        </div>
-                        <div>
-                            <p className='description-info block'>
-                                Please read all the information displayed on the website and on Ocasys prior to registration. If you still
-                                have questions, please send an email to the course email address.
-                                <br></br>
-                                <br></br>
-                                Note that there is a cap of 30 students so this course is only accessible to students who have been selected.
-                                For this reason, please mention all relevant experience when completing the registration form. (Deadline: 22 July)
-                                <div className='center'>
-                                    <button className='button'><a href='https://forms.gle/UHdoKkm1b6wzbHsb8' target="_blank" rel="noreferrer">Register</a></button>
-                                </div>
-                            </p>
-                        </div>
-                    </div>
-                    <div className='box' id='schedule'>
-                        <div>
-                            <p className='title-info red'>Ocasys</p>
-                        </div>
-                        <div>
-                            <div className='block description-info' >
-                                <p>
-                                    Link to the Ocasys page containing up-to-date information on the course.
-                                    <br></br>
-                                    <br></br>
-                                    Make sure to select the academic year 2025/26 when viewing this course on Ocasys.
-                                </p>
-                                <div className='center'>
-                                    <button className='button'><a href='https://ocasys.rug.nl/2025-2026/catalog/course/WBCS045-05' target="_blank" rel="noreferrer">Ocasys</a></button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+              <p className="title-info red">Registration</p>
+            </div>
+            <div>
+              <p className="description-info block">
+                Please read all the information displayed on the website and on
+                Ocasys prior to registration. If you still have questions,
+                please send an email to the course email address.
+                <br></br>
+                <br></br>
+                Note that there is a cap of 30 students so this course is only
+                accessible to students who have been selected. For this reason,
+                please mention all relevant experience when completing the
+                registration form. (Deadline: 12 July)
+                <div className="center">
+                  <button className="button">
+                    <a
+                      href="https://forms.gle/111RjQwyZ56pNZQi9
+"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Register
+                    </a>
+                  </button>
                 </div>
-
+              </p>
             </div>
-
-
+          </div>
+          <div className="box" id="schedule">
+            <div>
+              <p className="title-info red">Ocasys</p>
+            </div>
+            <div>
+              <div className="block description-info">
+                <p>
+                  Link to the Ocasys page containing up-to-date information on
+                  the course.
+                  <br></br>
+                  <br></br>
+                  Make sure to select the academic year 2026-2027 when viewing
+                  this course on Ocasys.
+                </p>
+                <div className="center">
+                  <button className="button">
+                    <a
+                      href="https://ocasys.rug.nl/2026-2027/catalog/course/WBCS045-05"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Ocasys
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Block3;
