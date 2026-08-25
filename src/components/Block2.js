784188f2a5a2d053ca3b9a36a74b3645c07ed7c7
Arend <arendspijker@gmail.com>
2026-08-25 14:01:31 +0200

    updated schedule

--- src/components/Block2.js
+++ src/components/Block2.js
@@ -16,27 +16,49 @@ const Block2 = () => {
             <div className="block">
               <p className="description-info ">
                 <ul className="list">
-                 <li>
-                  Lectures:
+                  <li>
+                    Lectures:
                     <ul>
-                      <li><strong>07/09/26</strong> -- Introduction</li>
-                      <li><strong>14/09/26</strong> -- Data Structures</li>
-                      <li><strong>21/09/26</strong> -- Greedy</li>
+                      <li>
+                        <strong>07/09/26</strong> -- Introduction
+                      </li>
+                      <li>
+                        <strong>11/09/26</strong> -- Data Structures
+                      </li>
+                      <li>
+                        <strong>23/09/26</strong> -- Greedy
+                      </li>
                     </ul>
                   </li>
-                    
-                  <li> Labs:
+
+                  <li>
+                    {" "}
+                    Labs:
                     <ul>
-                      <li><strong>16/09/26</strong> -- Data Structures</li>
-                      <li><strong>30/09/26</strong> -- Greedy</li>
-                      <li><strong>07/10/26</strong> -- TCR Usage</li>
+                      <li>
+                        <strong>16/09/26</strong> -- Data Structures
+                      </li>
+                      <li>
+                        <strong>30/09/26</strong> -- Greedy
+                      </li>
+                      <li>
+                        <strong>07/10/26</strong> -- TCR Usage
+                      </li>
                     </ul>
                   </li>
                   <li>
                     Contests:
-                    <ul><strong>19/09/26</strong> -- BAPC Prelims</ul>
-                    <ul><strong>TBD</strong> -- BAPC</ul>
-                    <ul><strong>12/10/26</strong> -- Internal Contest</ul>
+                    <ul>
+                      <li>
+                        <strong>26/09/26</strong> -- BAPC Prelims
+                      </li>
+                      <li>
+                        <strong>12/10/26</strong> -- Internal Contest
+                      </li>
+                      <li>
+                        <strong>21/10/26</strong> -- BAPC
+                      </li>
+                    </ul>
                   </li>
                 </ul>
               </p>
@@ -49,27 +71,52 @@ const Block2 = () => {
             <div>
               <p className="description-info block">
                 <ul className="list">
-                  <li> Lectures:
+                  <li>
+                    {" "}
+                    Lectures:
                     <ul>
-                      <li><strong>11/11/26</strong> -- TBD</li>
-                      <li><strong>02/12/26</strong> -- TBD</li>
-                      <li><strong>16/12/26</strong> -- TBD</li>
+                      <li>
+                        <strong>11/11/26</strong> -- TBD
+                      </li>
+                      <li>
+                        <strong>02/12/26</strong> -- TBD
+                      </li>
+                      <li>
+                        <strong>16/12/26</strong> -- TBD
+                      </li>
                     </ul>
-
                   </li>
-                  <li> Labs:
+                  <li>
+                    {" "}
+                    Labs:
                     <ul>
-                      <li><strong>18/11/26</strong> -- TDB</li>
-                      <li><strong>04/12/26</strong> -- TBD</li>
-                      <li><strong>18/12/26</strong> -- TBD</li>
+                      <li>
+                        <strong>18/11/26</strong> -- TDB
+                      </li>
+                      <li>
+                        <strong>04/12/26</strong> -- TBD
+                      </li>
+                      <li>
+                        <strong>18/12/26</strong> -- TBD
+                      </li>
                     </ul>
                   </li>
-                  <li> Contests:
+                  <li>
+                    {" "}
+                    Contests:
                     <ul>
-                      <li><strong>25/11/26</strong> -- Internal Contest</li>
-                      <li><strong>27-29/11/26</strong> -- NWERC</li>
-                      <li><strong>09/12/26</strong> -- Internal Contest</li>
-                      <li><strong>06/01/26</strong> -- Internal Contest</li>
+                      <li>
+                        <strong>25/11/26</strong> -- Internal Contest
+                      </li>
+                      <li>
+                        <strong>27-29/11/26</strong> -- NWERC
+                      </li>
+                      <li>
+                        <strong>09/12/26</strong> -- Internal Contest
+                      </li>
+                      <li>
+                        <strong>06/01/26</strong> -- Internal Contest
+                      </li>
                     </ul>
                   </li>
                 </ul>
